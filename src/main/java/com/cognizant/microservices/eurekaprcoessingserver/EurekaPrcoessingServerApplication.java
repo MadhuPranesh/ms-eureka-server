47fece0c0b1dd9cccef442441783766782c8e514
Madhu Pranesh <841223@cognizant.com>
2022-08-14 15:33:17 +0530

    Initial Project setup

---
+++ src/main/java/com/cognizant/microservices/eurekaprcoessingserver/EurekaPrcoessingServerApplication.java
@@ -0,0 +1,15 @@
+package com.cognizant.microservices.eurekaprcoessingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaPrcoessingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaPrcoessingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cognizant/microservices/eurekaprcoessingserver/EurekaPrcoessingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.microservices.eurekaprcoessingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaPrcoessingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
